caf482c89a2e505c6bc0772721f22403fdc3c022
c3farr <c3farr@gmail.com>
2023-11-18 14:50:32 -0700

    add database integration into project with initial scripts

---
+++ initialPyxelInsertion.js
@@ -0,0 +1,33 @@
+const db = require('./src/database.js');
+
+//FUll Image
+// for (let i = 0; i < 1280; i++) {
+//     for (let j = 0; j < 720; j++) {
+//         const pyxel = {
+//             id: `${i}-${j}`,
+//             locationX: i,
+//             locationY: j,
+//             color: "#FF0000",
+//             owner: ''
+//         };
+//         db.createPyxel(pyxel);
+//     }
+// }
+
+let idIterator = 0;
+
+//Top Left Corner
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    const pyxel = {
+      id: idIterator,
+      locationX: i,
+      locationY: j,
+      color: `#FF0000`,
+      owner: '',
+    };
+    db.createPyxel(pyxel);
+    idIterator++;
+    console.log('Created pyxel with id ' + pyxel.id);
+  }
+}
---
+++ src/database.js
@@ -0,0 +1,69 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('pyxel');
+const pyxelCollection = db.collection('pyxelData');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+async function createPyxel(pyxel) {
+  if ((await getPyxel(pyxel.id)).length === 0) {
+    const result = await pyxelCollection.insertOne(pyxel);
+    return result;
+  } else {
+    throw new Error('Pyxel already exists');
+  }
+}
+
+async function updatePyxel(pyxel) {
+  const query = { id: pyxel.id };
+  const newValues = { $set: {
+    locationX: pyxel.locationX,
+    locationY: pyxel.locationY,
+    color: pyxel.color,
+    price: pyxel.price,
+    owner: pyxel.owner,
+  } };
+  const result = await pyxelCollection.updateOne(query, newValues);
+  return result;
+}
+
+async function getPyxels() {
+  const cursor = pyxelCollection.find();
+  const array = await cursor.toArray();
+  return array;
+}
+
+async function getPyxel(id) {
+  const query = { id: id };
+  const cursor = pyxelCollection.find(query);
+  const array = await cursor.toArray();
+  if (array.length === 0) {
+    return null;
+  } else {
+    return array[0];
+  }
+}
+
+async function getPyxelsByOwner(owner) {
+  const query = { owner: owner };
+  const cursor = pyxelCollection.find(query);
+  return await cursor.toArray();
+}
+
+async function getPyxelsByLocation(locationX, locationY) {
+  const query = { locationX: locationX, locationY: locationY };
+  const cursor = pyxelCollection.find(query);
+  return await cursor.toArray();
+}
+
+module.exports = { createPyxel, updatePyxel, getPyxel, getPyxels, getPyxelsByOwner, getPyxelsByLocation };
